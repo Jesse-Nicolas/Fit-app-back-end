cc5d2d55dc1e48920410337a50d0d6b48b8ee3b9
Chrsteffen <Chrsteffen1@gmail.com>
2022-10-23 12:02:12 -0400

    meal index route stub

--- controllers/meals.js
+++ controllers/meals.js
@@ -1,7 +1,13 @@
 import { Meal } from '../models/meal.js'
 
-
+const index = async (req, res) => {
+  try {
+    console.log("Index works")
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
 
 export {
-
+  index,
 }
