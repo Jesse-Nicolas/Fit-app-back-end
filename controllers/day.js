96d213ee12916b3dbf60e93ec74e264170b087b4
Chrsteffen <Chrsteffen1@gmail.com>
2022-10-25 09:39:40 -0400

    create a day route

--- controllers/day.js
+++ controllers/day.js
@@ -1,7 +1,16 @@
 import { Day } from '../models/day.js'
 
-
+const create = async (req, res) => {
+  try {
+    req.body.author = req.user.profile
+    const day = await Day.create(req.body)
+    res.status(201).json(day)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
+}
 
 export {
-
+  create,
 }
--- controllers/profiles.js
+++ controllers/profiles.js
@@ -9,6 +9,15 @@ function index(req, res) {
     res.status(500).json(err)
   })
 }
+const show = async (req, res) => {
+  try {
+    const profile = await Profile.findById(req.params.id)
+    res.status(200).json(blog)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+}
+
 
 function addPhoto(req, res) {
   const imageFile = req.files.photo.path
@@ -29,4 +38,4 @@ function addPhoto(req, res) {
   })
 }
 
-export { index, addPhoto }
+export { index, addPhoto, show }
