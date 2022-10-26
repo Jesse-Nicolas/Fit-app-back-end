70f3e4d4addf6f50e5f5500b0892a52bbc7c57a8
James Hudson <jmh357@comcast.net>
2022-10-26 17:18:01 -0500

    save api data to meal database

--- models/meal.js
+++ models/meal.js
@@ -2,27 +2,16 @@ import mongoose from "mongoose"
 
 const Schema = mongoose.Schema
 
-const ingredientSchema = new Schema(
-  {
-    text: String,
-    quantity: Number,
-    measure: String,
-    food: String,
-    weight: Number,
-  }
-)
-
 const mealSchema = new Schema(
   {
     label: String,
-    ingredients: [ingredientSchema],
     description: String,
     calories: Number,
     image: String,
-    day: {
-      type: String,
-      enum: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'friday', 'Saturday'],
-    },
+    // day: {
+    //   type: String,
+    //   enum: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'friday', 'Saturday'],
+    // },
   }
 )
 
